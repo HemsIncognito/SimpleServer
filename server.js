c7ea3ba14e33165f52ea34aedcc0f40877659137
JackHarperIMF <hemvayyala8@gmail.com>
2024-03-17 12:46:39 +0530

    simpleServer

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const http = require("node:http");
+const http = require("http");
 
-const PORT = 8081;
+const PORT = 8082;
 
-const server = http.createServer((res,req) =>{
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Welcome!</h1>");
+const toDoList = ["Hello!", "this is just a", "Database"];
+
+const server = http.createServer((req,res) =>{
+    const {method, url} = req;
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            // res.statusCode(200);
+            res.write(toDoList.toString());
+        }else if(method === "POST"){
+            let task = "";
+            req.on('error', err => {
+                console.log(err);
+            }).on('data', (chunck) => {
+                task += chunck;
+            }).on('end', () => {
+                body = JSON.parse(body);
+                let newTodo = toDoList;
+                newTodo.push(body.item);
+                console.log(newTodo);
+            })
+        }else if(method === "DELETE"){
+
+        }
+    }
+    else{
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write("Welcome!");
+       
+    }
     res.end();
 });
 
 server.listen(PORT, () => {
     console.log(`Node.js server is up amnd running on Port: ${PORT}`);
-    // or use console.log("Node.js server is up amnd running on Port:", PORT);
+    // or use console.log("Node.js server is up and running on Port:", PORT);
 });
